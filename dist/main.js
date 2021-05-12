af873ee4448cf37f141daf68edd4acebf27d5d12
Joel <j_90@live.co.uk>
2021-05-12 01:07:56 +0100

    modifies js & css animations

--- dist/main.js
+++ dist/main.js
@@ -17,23 +17,69 @@ const navObserver = new IntersectionObserver((entries, navObserver) => {
 
 navObserver.observe(hero);
 
-const faders = document.querySelectorAll(".fade-in");
+// About Images
 
-const fadeInOptions = {
+const slideInLeft = document.querySelectorAll(".slide__in--left");
+
+const slideInLeftOptions = {
   rootMargin: "0px 0px -200px 0px",
 };
 
-const fadeInScroll = new IntersectionObserver((entries, fadeInScroll) => {
+const slideInLeftScroll = new IntersectionObserver((entries, slideInLeftScroll) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
     } else {
-      entry.target.classList.add("appear");
-      fadeInScroll.unobserve(entry.target);
+      entry.target.classList.add("left-appear");
+      slideInLeftScroll.unobserve(entry.target);
     }
   });
-}, fadeInOptions);
+}, slideInLeftOptions);
 
-faders.forEach((fader) => {
-  fadeInScroll.observe(fader);
+slideInLeft.forEach((slide) => {
+  slideInLeftScroll.observe(slide);
+});
+
+const slideInRight = document.querySelectorAll(".slide__in--right");
+
+const slideInRightOptions = {
+  rootMargin: "0px 0px -200px 0px",
+};
+
+const slideInRightScroll = new IntersectionObserver((entries, slideInRightScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("right-appear");
+      slideInRightScroll.unobserve(entry.target);
+    }
+  });
+}, slideInRightOptions);
+
+slideInRight.forEach((slide) => {
+  slideInRightScroll.observe(slide);
+});
+
+// Testimonial Cards
+
+const dropIn = document.querySelectorAll(".drop__in");
+
+const dropInOptions = {
+  rootMargin: "0px 0px -200px 0px",
+};
+
+const dropInScroll = new IntersectionObserver((entries, dropInScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("drop-appear");
+      dropInScroll.unobserve(entry.target);
+    }
+  });
+}, dropInOptions);
+
+dropIn.forEach((drop) => {
+  dropInScroll.observe(drop);
 });
